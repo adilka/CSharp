9315162515bf86fcdd1a18170cbe47a7d2b9c74b
Adil <adil.manakbayev@gmail.com>
2022-11-27 22:20:45 +0600

    Мой первый коммит с задачаей

---
+++ HM1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Моя первая решенная задача
+Console.Clear();
+Console.Write("Vvedite Pervoe Chislo: ");
+Console.Write("Vvedite Vtoroe Chislo: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+
+ if (b * b == a){
+
+ Console.WriteLine("yes");
+ }
+ else
+ {
+    Console.WriteLine("NO");
+ }
